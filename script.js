787efb17759d206dcf5c0aa25a69789a741007a7
knika-starkey <knika787@gmail.com>
2022-10-08 01:54:51 +0300

    added functiouns

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
-let m;
-let robot;
-let n = prompt("");
-let str = "";
-while (n > 0) {
-  for (let i = 0; i < n; i++) {
-    str += "| ";
-  }
-  alert(str);
-  str = "";
-  alert("Your turn!");
-  m = prompt("Get the matches!");
-  /** todo isNaN() m <= 0 or m>3 */
-  if (isNaN(m) || m <= 0 || m > 3) {
-    alert("Wrong number");
-    continue;
-  }
-  n -= m;
+let str;
+function matches() {
+  let m;
+  let robot;
+  let n = prompt("How many matches?");
 
-  if (n <= 0) {
-    alert("Your win!");
-    break;
-  }
-  alert("My turn!");
-  robot = (n - 1) % 4 == 0 ? 1 : (n - 3) % 4 == 0 ? 3 : 2;
-  /* todo */
-  n -= robot;
-  if (n <= 0) {
-    alert("I win!");
-    break;
+  while (n > 0) {
+    alert(show(n));
+    alert("Your turn!");
+    m = prompt("Get the matches!");
+    /** todo isNaN() m <= 0 or m>3 */
+    if (isNaN(m) || m <= 0 || m > 3) {
+      alert("Wrong number");
+      continue;
+    }
+    n -= m;
+    alert(show(n));
+    if (n <= 0) {
+      alert("Your win!");
+      return;
+    }
+    alert("My turn!");
+    robot = (n - 1) % 4 == 0 ? 1 : (n - 3) % 4 == 0 ? 3 : 2;
+    /* todo */
+    n -= robot;
+    show(n);
+    if (n <= 0) {
+      alert("I win!");
+      return;
+    }
   }
 }
+function show(amount) {
+  str = amount + ": ";
+  for (let i = 0; i < amount; i++) {
+    str += "| ";
+  }
+  return str;
+}
+matches();
